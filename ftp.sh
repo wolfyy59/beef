1b2e0d67490be0231073d0e5878d852f40914a24
wolfyy <60975463+wolfyy59@users.noreply.github.com>
2022-11-16 14:43:56 +0100

    Create ftp.sh

--- ftp.sh
+++ ftp.sh
@@ -10,11 +10,7 @@ mput 70.txt
 bye
 EOF
 clear
-rm 2022.tar 
-rm 70.txt
-rm -rif 77/
-rm ftp.sh
-rm config.sh 
+
 
 # where i = interactive mode / -n = auto login / -v = verbose
 # EOF means this script will execute the below lines until it gets another EOF
