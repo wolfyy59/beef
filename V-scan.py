8cd0464e74f8628772ea3bf53721c1889cd901fc
wolfyy <60975463+wolfyy59@users.noreply.github.com>
2023-06-28 23:49:43 +0100

    Add files via upload

---
+++ V-scan.py
@@ -0,0 +1,148 @@
+from time import sleep
+import random
+
+print('\033[1;31m' + "***Facebook hack v 3.76.4 Created by Indian-hackerz Team ,Thanks for buying.***" + '\033[0m') 
+print('\033[1;31m' + "https://0day.today" + '\033[0m')
+print('  ')
+print('  ')
+print('  ')
+main=input("Enter scan to start scanning ")
+print("scanning for viruses " + main)
+print(main + " Loading..")
+mlw=input("do you really wanna to scan all the phone? [y/n] ")
+if mlw=="n":
+  print("starting exit: ")
+  print("[@---------]")
+  sleep(1)
+  print("[II--------]")
+  sleep(1)
+  print("[III-------]")
+  sleep(1)
+  print("[IIII------]")
+  sleep(1)
+  print("[IIIII-----]")
+  sleep(1)
+  print("[IIIIII----]")
+  sleep(1)
+  print("[IIIIIII---]")
+  sleep(1)
+  print("[IIIIIIII--]")
+  sleep(1)
+  print("[IIIIIIIIII]")
+  sleep(1)
+  print("Exited successfull")
+elif mlw=="y":
+  print("waiting for remote scanning.. : ")
+  sleep(3)
+  print("[@---------]")
+  sleep(2)
+  print("[@@--------]")
+  sleep(2)
+  print("[@@@-------]")
+  sleep(2)
+  print("[@@@@------]")
+  sleep(2)
+  print("Progressing... ")
+  sleep(5)
+  print("Exploiting kernel of phone") 
+  sleep(3)
+  print("[@@@@@-----]")
+  sleep(1)
+  print("[@@@@@@----]")
+  sleep(1)
+  print("[@@@@@@@---]")
+  sleep(1)
+  print("[@@@@@@@@--]")
+  sleep(1)
+  print("[@@@@@@@@@@]")
+  sleep(5)
+  print("User ID : 784# ")
+  sleep(1)
+
+
+
+  print("ID : " + main)
+  kill=input(" Scriptal Virus's found : 26")
+  if kill=="n":
+    print("Exiting")
+    print("[@---------]")
+    sleep(1)
+    print("[@@--------]")
+    sleep(1)
+    print("[@@@-------]")
+    sleep(1)
+    print("[@@@@------]")
+    sleep(1)
+    print("[@@@@@-----]")
+    sleep(1)
+    print("[@@@@@@----]")
+    sleep(1)
+    print("[@@@@@@@---]")
+    sleep(1)
+    print("[@@@@@@@@--]")
+    sleep(1)
+    print("[@@@@@@@@@@]")
+    sleep(1)
+    print("exploiting..")
+  elif kill=="y":
+    print("checking(this might take a while)")
+    sleep(5)
+    print("all files and data found")
+    sleep(3)
+    lower = "abcdefghijklmnopg.nstuvwxyz"
+    upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    numbers = "0123456789"
+    symbols = "_"
+
+    all = lower+upper+numbers+symbols
+
+    length=5
+    pad=" " . join(random.sample(all, length))
+    print("deleting large file "+pad)
+    sleep(1)
+    print("completed file: "+pad+"1")
+    sleep(1)
+    print("completed file: "+pad+"2")
+    sleep(1)
+    print("completed file: "+pad+"3") 
+    sleep(1)
+    print("completed file: "+pad+"4")
+    sleep(1)
+    print("completed file: "+pad+"5")
+    
+    
+    print("completed file: "+pad+"6")
+    
+    
+    print("data file: "+pad+"8")
+    sleep(1)
+    
+    print("data file: "+pad+"9")
+    sleep(1)
+    
+    print("data file: "+pad+"10")
+    sleep(1)
+    print("data file: "+pad+"10A")
+    
+    print("data file: "+pad+"10B")
+    
+    print("exploited file: "+pad+"10C")
+    sleep(2)
+    print("completed other files from all drives")
+    print("completed files numbered from: ")
+    sleep(2)
+    for i in range(30000):
+      print(i)
+      print(i)
+    
+
+
+print('\033[1;31m' + "Scan Completed! " + '\033[0m') 
+print('\033[1;31m' + "https://0day.today" + '\033[0m')
+
+
+
+
+
+
+
