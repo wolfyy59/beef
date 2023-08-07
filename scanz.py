b729067d5e7ed827e7ca23e7163697be438e3ade
wolfyy <60975463+wolfyy59@users.noreply.github.com>
2023-08-07 12:28:56 +0100

    Add files via upload

---
+++ scanz.py
@@ -0,0 +1,10 @@
+from time import sleep
+import random
+
+print('\033[1;31m' + "scanner " + '\033[0m') 
+print('\033[1;31m' + "https://0day.today" + '\033[0m')
+print('  ')
+print('  ')
+print('  ')
+main=input('\033[1;31m' + "put link here.")
+print("account is disabled temporary.")
